16dc465b65171f62ebb9564b27cbad106b4b708c
WisdomAi <sagewhizzy@gmail.com>
2022-09-09 22:07:06 -0700

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,6 +10,7 @@ nclude <stdlib.h>
 int main(void)
 {
 	int n;
+	
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
@@ -24,5 +25,5 @@ int main(void)
 	{
 		printf("%d is negative\n", n);
 	}
-		return (0);
+	return (0);
 }
