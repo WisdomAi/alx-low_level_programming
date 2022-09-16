3763bba59382bd9c155a1de0cc4bd807e24a17d2
WisdomAi <sagewhizzy@gmail.com>
2022-09-16 13:28:24 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,24 +1,31 @@
 #include "main.h" 
   
- /** 
-  * main - prints Putchar 
-  * 
-  * Description: prints _putchar 
-  * 
-  * Return: 0 (Success) 
-  */ 
+/** 
+ * main - Entry point 
+ * 
+ * Return: nothing, return void 
+ */ 
   
- int main(void) 
- { 
-         _putchar('_'); 
-         _putchar('p'); 
-         _putchar('u'); 
-         _putchar('t'); 
-         _putchar('c'); 
-         _putchar('h'); 
-         _putchar('a'); 
-         _putchar('r'); 
-         _putchar('\n'); 
+int main(void) 
+ 
+{ 
+        _putchar('_'); 
+
+        _putchar('p'); 
   
-         return (0); 
- }
+        _putchar('u'); 
+  
+        _putchar('t'); 
+  
+        _putchar('c'); 
+  
+        _putchar('h'); 
+  
+        _putchar('a'); 
+  
+        _putchar('r'); 
+  
+        _putchar('\n'); 
+  
+        return (0); 
+}
