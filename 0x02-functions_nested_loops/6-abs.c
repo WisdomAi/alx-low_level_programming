5127a12018e580018f7e7d04e712f89fa549f6f1
WisdomAi <sagewhizzy@gmail.com>
2022-09-17 21:30:38 -0700

    Task 6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-"include <stdio.h>
+#include <stdio.h>
 
 /**
  * _abs - value absolute
@@ -8,10 +8,10 @@
  * Return: Always 0.
  */
 
-int _abs(int n)
+int  _abs(int  n)
 
 {
-	if (n < 0)
+	if(n<0)
 	{
 		return (n * (-1));
 	}
