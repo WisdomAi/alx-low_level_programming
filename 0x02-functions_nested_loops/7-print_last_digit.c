3bce769eca3b9441b006c122bd515b3a46c6e386
WisdomAi <sagewhizzy@gmail.com>
2022-09-23 08:37:36 -0700

    Task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,32 +1,23 @@
 #include "main.h"
+/**
+ * print_last_digit - print the last digit of a number
+ * @n: int type number
+ * Return: return value of last digit
+ */
+int print_last_digit(int n)
+{
+	int r;
 
-: /**
-  * print_last_digit - last digit
-  * @n: The int to print
-  * Return: Always 0.
-  */
-
- int print_last_digit(int n)
-
- {
-         int last_digit;
-
-         if (n < 0)
-
-         {
-                 last_digit = (-1 * (n % 10));
-
-                 _putchar (last_digit + '0');
-
-                 return (last_digit);
-         }
-         else
-         {
-                 last_digit = (n % 10);
-
-                 _putchar (last_digit + '0');
-
-                 return (last_digit);
-         }
- }
-
+	if (n < 0)
+	{
+		r = -1 * (n % 10);
+		_putchar(r + '0');
+		return (r);
+	}
+	else
+	{
+		r = n % 10;
+		_putchar(r + '0');
+		return (r);
+	}
+}
