3c3fd962dba2fe281821d57cb13b362c71c47fd1
WisdomAi <sagewhizzy@gmail.com>
2022-09-23 08:29:07 -0700

    Task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,32 +1,26 @@
 #include "main.h"
-
 /**
- * print_sign - return 0 letter not lowercase, 1 letter lowecase
- * @n: the int to print
- * Return: Always 0.
+ * print_sign - Print the sign of a number
+ * @n: type int integer, can be negative or positive
+ * Description: print +, 0, or - depending on number, along with return
+ * Return: 1 if +, 0 if 0, and -1 if -
  */
 
 int print_sign(int n)
-
 {
 	if (n > 0)
 	{
-		_putchar ('+');
-
+		_putchar('+');
 		return (1);
-
 	}
-	else if (n == 0)
+	else if (n < 0)
 	{
-
-		_putchar ('0');
-
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
 		return (0);
-		{
-		else
-		}
-			_putchar ('-')
-
-			return (-1)
-		}
+	}
 }
