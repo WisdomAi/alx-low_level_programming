cbdf03998f4fa3cf30854396f266a1d2b53759bd
WisdomAi <sagewhizzy@gmail.com>
2022-09-23 21:56:33 -0700

    Taak 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,6 +1,7 @@
-"include "main.h"
+#include "main.h"
+
 /**
-* main - tests function that prints if integer is positive or negative
+ * main - tests function that prints if integer is positive or negative
  * Return: 0
  */
 
@@ -10,31 +11,6 @@ int main(void)
 
 	i = 0;
 	positive_or_negative(i);
-
-	return (0);
-
-}
-#include <stdio.h>
-
-/**
- * main - causes an infinite loop
- * Return: 0
- */
-
-int main(void)
-{
-	int i;
-
-	printf("Infinite loop incoming : (\n");
-
-	i = 0;
-
-	/*while (i < 10)*/
-	/*{*/
-	/*putchar(i);*/
-	/*}*/
-
-	printf("Infinite loop avoided !\\0   /  \n");
 
 	return (0);
 }
