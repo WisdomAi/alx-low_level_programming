9f8b030b27b797a247a1e01f45840edef2624da2
WisdomAi <sagewhizzy@gmail.com>
2022-10-28 12:52:40 -0700

    Manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,20 +1,20 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-/*
- * File: holberton.h
- * AUth: Wisdom Ai
- * Desc: Header file containing prototypes for all functions
- *       written in the 0x13-bit_manipulation directory.
+#include "main.h"
+/**
+ * binary_to_uint - converts a binary number to unsigned int
+ * @b: string containing the binary number
+ * Return: the converted number
  */
-
-int _putchar(char c);
-unsigned int binary_to_uint(const char *b);
-void print_binary(unsigned long int n);
-int get_bit(unsigned long int n, unsigned int index);
-int set_bit(unsigned long int *n, unsigned int index);
-int clear_bit(unsigned long int *n, unsigned int index);
-unsigned int flip_bits(unsigned long int n, unsigned long int m);
-int get_endianness(void);
-
-#endif /* MAIN_H */
+unsigned int binary_to_uint(const char *b)
+{
+int i;
+unsigned int digit = 0;
+if (!b)
+return (0);
+for (i = 0; b[i]; i++)
+{
+if (b[i] != '0' && b[i] != '1')
+return (0);
+digit = 2 * digit + (b[i] - '0');
+}
+return (digit);
+}
